ac2b84e6c5bbca01f2fcb16b2c4cd9c381bb4adb
LHAM99 <luis_heber9@hotmail.com>
2023-10-13 13:32:24 -0600

    Models Unit Testing

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -10,3 +10,6 @@ class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits = 6, decimal_places = 2, db_index=True)
     inventory = models.SmallIntegerField()
+    
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
