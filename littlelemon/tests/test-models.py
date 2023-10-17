d88527b160bbe7061b90aa3951a997838cf17688
LHAM99 <luis_heber9@hotmail.com>
2023-10-17 11:38:41 -0600

    Adding MenuItems to Admin Panel

--- littlelemon/tests/test-models.py
+++ littlelemon/tests/test-models.py
@@ -6,3 +6,11 @@ class MenuItemTest(TestCase):
     def test_get_item(self):
         item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
         self.assertEqual(item, "IceCream : 80")
+
+class MenuViewTest(TestCase):
+    def setUp(self) -> None:
+        item1 = MenuItem.objects.create(title="IceCream1", price=80, inventory=100)
+        item2 = MenuItem.objects.create(title="IceCream2", price=80, inventory=100)
+        self.assertEqual(item1, "IceCream1 : 80")
+        self.assertEqual(item2, "IceCream1 : 80")
+        
